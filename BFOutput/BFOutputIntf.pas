55bdecb70d2c54362f5a9aff227c6dacdcfe0c09
Nuno Picado <geral@nunopicado.com>
2016-12-20 00:58:26 +0000

    [BFOutput] Fix: Indentation optimization

--- BFOutput/BFOutputIntf.pas
+++ BFOutput/BFOutputIntf.pas
@@ -4,10 +4,10 @@ interface
 
 type
     IBFCell = Interface ['{68191C4D-9539-4730-B4AB-3C423145376C}']
-      function Add  : IBFCell;
-      function Sub  : IBFCell;
-      function Value: Byte;
-      function Define(NewValue: Byte): IBFCell;
+      function Add                    : IBFCell;
+      function Sub                    : IBFCell;
+      function Value                  : Byte;
+      function Define(NewValue: Byte) : IBFCell;
     End;
 
     IBFStack = Interface ['{0149AEDA-BF0B-481C-9227-C8710CDC2AAF}']
