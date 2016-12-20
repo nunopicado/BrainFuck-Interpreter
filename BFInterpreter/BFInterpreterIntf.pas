aa158dafb33d28318e7dbf13e6fa6c1acae8ee0e
Nuno Picado <geral@nunopicado.com>
2016-12-20 00:33:44 +0000

    [BFInterpreter] Fix: Indentation optimization

--- BFInterpreter/BFInterpreterIntf.pas
+++ BFInterpreter/BFInterpreterIntf.pas
@@ -8,8 +8,8 @@ uses
 
 type
     IBFInterpreter = Interface ['{56E9D1C7-756B-4C96-9F7F-A66FE2A5BE06}']
-      function Run: IBFInterpreter;
-      function AsString: String;
+      function Run      : IBFInterpreter;
+      function AsString : String;
     End;
 
 implementation
